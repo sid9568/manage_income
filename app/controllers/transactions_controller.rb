96035d6cc12c2f615663266e15e8950b4635cf50
sid9568 <sid20319@gmail.com>
2025-04-16 13:41:45 +0530

    Added dahsboard index and curd

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,13 +1,44 @@
 class TransactionsController < ApplicationController
+
   def index
+    @transactions = Transaction.all
   end
 
   def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    if @transaction.save
+      redirect_to transactions_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
+  def update
+    if @transaction.update(transaction_params)
+      redirect_to transactions_path
+    else
+      render :edit
+    end
+  end
+
   def show
   end
+
+  private
+
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:title, :amount, :category, :date, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'dashboards#index'
 
-  root "transactions#index"
   resources :transactions
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
